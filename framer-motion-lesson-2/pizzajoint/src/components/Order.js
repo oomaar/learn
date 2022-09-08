10ad15d533747a6dbd21388d6ba0b1b4afbf2bcb
oomaar <omaar_5@hotmail.com>
2022-09-08 20:23:03 +0200

    learn how to use framer motion libraray

---
+++ framer-motion-lesson-2/pizzajoint/src/components/Order.js
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+const containerVarients = {
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visable: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      mass: 0.4,
+      damping: 8,
+      when: "beforeChildren",
+      staggerChildren: 0.4
+    }
+  },
+  exit: {
+    x: '-100vw',
+    transition: { ease: 'easeInOut' }
+  }
+};
+
+const childVarient = {
+  hidden: {
+    opacity: 0
+  },
+  visable: {
+    opacity: 1
+  }
+};
+
+const Order = ({ pizza, setShowModal }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      setShowModal(true);
+    }, 5000);
+  }, [setShowModal]);
+
+  return (
+    <motion.div
+      className="container order"
+      variants={containerVarients}
+      initial="hidden"
+      animate="visable"
+      exit="exit"
+    >
+      <h2>Thank you for your order :)</h2>
+      <motion.p variants={childVarient}>You ordered a {pizza.base} pizza with:</motion.p>
+      <motion.div variants={childVarient}>
+        {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Order;
